a7f78509264d3b34366693fc6f84e896cd0ff85f
tec Santosh Kumar <62633516+tecsantoshkumar@users.noreply.github.com>
2022-07-24 08:32:16 +0530

    Define ESP D1 pin as a LED pin

--- esp8266_blink.ino
+++ esp8266_blink.ino
@@ -1,14 +1,8 @@
 /*
- ESP8266 Blink by Simon Peter
- Blink the blue LED on the ESP-01 module
- This example code is in the public domain
- 
- The blue LED on the ESP-01 module is connected to GPIO1 
- (which is also the TXD pin; so we cannot use Serial.print() at the same time)
- 
- Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
+@ author : Tec Santosh  Kmuar
+@ version : 1.0
 */
-#define LED_PIN D1
+#define LED_PIN D1  //Define ESP D1 pin as a LED pin
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
